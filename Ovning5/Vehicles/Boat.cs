d04967f1a4481ff08e444357ab22c85feac4eeec
jarethholt <jareth.holt@gmail.com>
2024-03-19 15:10:09 +0100

    feat: Add Boat class

---
+++ Ovning5/Vehicles/Boat.cs
@@ -0,0 +1,22 @@
+﻿namespace Ovning5.Vehicles;
+
+public class Boat : Vehicle
+{
+    private const float _metersToFeet = 3.28084f;
+
+    public override int NumberOfWheels { get; init; } = 0;
+    public override string Color { get; protected set; } = "White";
+    public int LengthInMeters { get; init; }
+    public int LengthInFeet => (int)(LengthInMeters * _metersToFeet);
+    public string Name { get; private set; } = "(Unnamed)";
+
+    public Boat(int registrationNumber, int lengthInMeters)
+    {
+        RegistrationNumber = registrationNumber;
+        LengthInMeters = lengthInMeters;
+    }
+
+    public void ChristenBoat(string name) => Name = name;
+
+    public override string ToString() => $"{LengthInMeters}-m {Color} boat: {Name}";
+}
