00100a1d3fbedc9b97907e6bac622f1cfd5f9393
jarethholt <jareth.holt@gmail.com>
2024-03-19 13:40:39 +0100

    Add project files.

---
+++ Ovning5.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Ovning5.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Ovning5/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Ovning5;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
