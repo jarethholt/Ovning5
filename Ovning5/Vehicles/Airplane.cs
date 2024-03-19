30d0d8ecfdea3b3b6496b31fbc4a24235f881747
jarethholt <jareth.holt@gmail.com>
2024-03-19 15:24:52 +0100

    feat: Add Airplane class

---
+++ Ovning5/Vehicles/Airplane.cs
@@ -0,0 +1,27 @@
+﻿namespace Ovning5.Vehicles;
+
+public class Airplane : Vehicle
+{
+    public override string Color { get; protected set; } = "White";
+    public string Make { get; init; }
+    public string Model { get; init; }
+    public string AirplaneType { get; init; }
+    public int PassengerCapacity { get; init; }
+
+    public Airplane(
+        int registrationNumber,
+        string make,
+        string model,
+        string airplaneType,
+        int passengerCapacity
+    )
+    {
+        RegistrationNumber = registrationNumber;
+        Make = make;
+        Model = model;
+        AirplaneType = airplaneType;
+        PassengerCapacity = passengerCapacity;
+    }
+
+    public override string ToString() => $"{Color} {Make} {Model}";
+}
