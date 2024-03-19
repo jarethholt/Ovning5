4b3d1a12b9712ea4c89bd18cecf3886c2ea5fd61
jarethholt <jareth.holt@gmail.com>
2024-03-19 22:13:12 +0100

    feat: Add a Registry class
    Add the Registry class based on HashSet<RegistrationCode>. Provides methods Contains, Add, Remove, and GenerateNewRegistrationCode.

---
+++ Ovning5/Registration/Registry.cs
@@ -0,0 +1,40 @@
+﻿namespace Ovning5.Registration;
+
+public class Registry
+{
+    private readonly HashSet<RegistrationCode> _registrationCodes = [];
+
+    public bool Contains(RegistrationCode registrationCode)
+        => _registrationCodes.Contains(registrationCode);
+
+    public bool Contains(string code)
+    {
+        try
+        {
+            RegistrationCode registrationCode = new(code);
+            return Contains(registrationCode);
+        }
+        catch (ArgumentException)
+        {
+            return false;
+        }
+    }
+
+    public bool Add(RegistrationCode registrationCode)
+        => _registrationCodes.Add(registrationCode);
+
+    public bool Remove(RegistrationCode registrationCode)
+        => _registrationCodes.Remove(registrationCode);
+
+    public RegistrationCode GenerateNewRegistrationCode(Random random)
+    {
+        RegistrationCode registrationCode;
+        bool isNew;
+        do
+        {
+            registrationCode = RegistrationCode.GenerateCode(random);
+            isNew = Contains(registrationCode);
+        } while (!isNew);
+        return registrationCode;
+    }
+}
