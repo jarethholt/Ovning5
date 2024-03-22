9817ad257f2c21115768eef605a72b7a0258f07c
jarethholt <jareth.holt@gmail.com>
2024-03-22 06:02:14 +0100

    feat: Create GarageHandler

---
+++ Ovning5/VehicleCollections/GarageHandler.cs
@@ -0,0 +1,29 @@
+﻿using Ovning5.Vehicles;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Ovning5.VehicleCollections;
+
+public class GarageHandler(int maxCapacity) : IGarage<Vehicle>
+{
+    private Garage<Vehicle> _garage = new(maxCapacity);
+
+    public int Count => _garage.Count;
+
+    public bool HasSpace => _garage.HasSpace;
+
+    public int MaxCapacity => _garage.MaxCapacity;
+
+    public bool FindByID(string vehicleID, [MaybeNullWhen(false)] out Vehicle vehicle)
+        => _garage.FindByID(vehicleID, out vehicle);
+
+    public IEnumerator<Vehicle?> GetEnumerator() => _garage.GetEnumerator();
+
+    public string ListAll() => _garage.ListAll();
+
+    public bool Remove(Vehicle vehicle) => _garage.Remove(vehicle);
+
+    public bool TryAdd(Vehicle vehicle) => _garage.TryAdd(vehicle);
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
